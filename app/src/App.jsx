2b798fd62ccc182370240a0fb95c92e193b35719
royinbris <royinbris@gmail.com>
2026-06-26 15:26:12 +1000

    debug: 주메뉴 상단 차단 레이어 파란색으로 시각화 (임시)

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -604,7 +604,7 @@ function GlobalBottomBar() {
         }}
       >
         {/* 상단 6px 터치 차단 레이어 */}
-        <div style={{ position: 'absolute', top: 0, left: 0, right: 0, height: '6px', zIndex: 10, pointerEvents: 'all' }} />
+        <div style={{ position: 'absolute', top: 0, left: 0, right: 0, height: '6px', zIndex: 10, pointerEvents: 'all', backgroundColor: 'rgba(0, 100, 255, 0.5)' }} />
         {/* ── 하단막대 본체 ── */}
         <div
           className="global-bottom-bar"
